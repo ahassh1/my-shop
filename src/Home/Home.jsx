8842b16cbb555ebd5b477676d7f485aa191e4b6c
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-08-09 04:10:28 +0600

    footer section done

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Banner from "../Banner/Banner";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
   return (
@@ -8,6 +9,9 @@ const Home = () => {
       <Navbar></Navbar>
       <div className="md:p-24 md:bg-sky-200 bg-sky-100">
         <Banner></Banner>
+      </div>
+      <div className="my-5">
+        <Footer></Footer>
       </div>
     </div>
   );
